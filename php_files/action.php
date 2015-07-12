9d73a363ec3488fd7682e9372dfb5d9d8b8d9ee1
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2015-07-12 17:09:18 -0500

    adds css and php files

---
+++ php_files/action.php
@@ -0,0 +1,26 @@
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+include "$root/include_files/heading.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta property="og:title" content="Greatest Games" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://www.greatestgamez.com/" />
+<meta property="og:image" content="http://www.greatestgamez.com" />
+<meta property="fb:admins" content="100007419488033" />
+<title>Greatest Games</title>
+<link rel="stylesheet" type="text/css" href="http://www.greatestgamez.com/css_files/homestylecss.css"/>
+<link rel="shortcut icon" href="http://www.greatestgamez.com/images/favicon.ico">
+</head>
+<body>
+<?php generateHeader("action");?>
+<p></p>
+<p>
+<a href="http://www.greatestgamez.com/action_games/template.php"><h1>GALAXY SHOOT</h1></a>
+</p>
+<?php generateFooter();?>
+</body>
+</html>
---
+++ php_files/mobile.php
@@ -0,0 +1,23 @@
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+include "$root/include_files/heading.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta property="og:title" content="Greatest Games" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://www.greatestgamez.com/" />
+<meta property="og:image" content="http://www.greatestgamez.com" />
+<meta property="fb:admins" content="100007419488033" />
+<title>Greatest Games</title>
+<link rel="stylesheet" type="text/css" href="http://www.greatestgamez.com/css_files/homestylecss.css"/>
+<link rel="shortcut icon" href="http://www.greatestgamez.com/images/favicon.ico">
+</head>
+<body>
+<?php generateHeader("mobile");?>
+<p><embed src="http://games.mochiads.com/c/g/bloons-tower-defense-3-distribute/bloonstd3.swf" menu="false" quality="high" width="640" height="480" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></p>
+<?php generateFooter();?>
+</body>
+</html>
